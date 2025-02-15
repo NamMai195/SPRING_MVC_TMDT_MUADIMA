51a00666d22377ca90164e7c75b4eb4ad90f0a3a
NamMai195 <mnam3239@gmaill.com>
2025-02-15 13:10:56 -0500

    update AdminRepository and UserRepository

---
+++ src/main/java/com/spring/mvc/repository/AdminRepository.java
@@ -0,0 +1,18 @@
+package com.spring.mvc.repository;
+
+import com.spring.mvc.domain.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+    Admin save(Admin admin);
+
+    void deleteById(long id);
+
+    List<Admin> findAll();
+
+    Admin findByid(long id);
+}
---
+++ src/main/java/com/spring/mvc/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.spring.mvc.repository;
+
+import com.spring.mvc.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    User save(User user);
+
+    void deleteById(long id);
+
+    List<User> findAll();
+
+    User findById(long id);
+}
