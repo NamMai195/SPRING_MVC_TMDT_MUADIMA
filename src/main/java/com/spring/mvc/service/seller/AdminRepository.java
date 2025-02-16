a6bac57a2729f6801eacbcc803b7c7103ead41c2
NamMai195 <mnam3239@gmaill.com>
2025-02-16 11:33:40 -0500

    update thumuc 1.1

---
+++ src/main/java/com/spring/mvc/service/seller/AdminRepository.java
@@ -0,0 +1,20 @@
+package com.spring.mvc.service.seller;
+
+import com.spring.mvc.domain.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+    Admin save(Admin admin);
+
+    void deleteById(long id);
+
+    List<Admin> findAll();
+
+    Admin findByid(long id);
+
+    Admin findByUsername(String username);
+}
